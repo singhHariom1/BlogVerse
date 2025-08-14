6d0bd5d83425bb5bce398d83980ccd582b85b137
Hariom Singh <singh.hariom6199@gmail.com>
2025-08-14 23:29:33 +0530

    Day 45.

--- server/server.js
+++ server/server.js
@@ -5,4 +5,6 @@ const PORT=5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+app.get("/api", (req, res) => res.send("Hello World!"));
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
